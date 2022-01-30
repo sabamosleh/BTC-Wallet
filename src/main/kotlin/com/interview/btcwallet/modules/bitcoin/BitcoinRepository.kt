eea2ffd9d604cc83470e61bcd46f83c64c13eabf
saba <moslehsaba@gmail.com>
2022-01-30 11:38:25 +0330

    add repository

---
+++ src/main/kotlin/com/interview/btcwallet/modules/bitcoin/BitcoinRepository.kt
@@ -0,0 +1,9 @@
+package com.interview.btcwallet.modules.bitcoin
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
+
+@Repository
+@Transactional
+interface BitcoinRepository : JpaRepository<BitcoinEntity, Long>
