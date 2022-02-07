a6b60c2e4cba3723bf48022892a04681b0723428
saba <moslehsaba@gmail.com>
2022-02-08 02:04:42 +0330

    created view class for wallet status

--- src/main/kotlin/com/interview/btcwallet/modules/wallet/WalletView.kt
+++ src/main/kotlin/com/interview/btcwallet/modules/wallet/WalletView.kt
@@ -1,4 +1,6 @@
 package com.interview.btcwallet.modules.wallet
 
-class WalletView {
-}
+import java.math.BigDecimal
+import java.util.*
+
+data class WalletView(val amount: BigDecimal?, val dateTime: Date?)
